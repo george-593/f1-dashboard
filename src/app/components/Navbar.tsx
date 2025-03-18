ecdaf9969ea45d434845a204c519de70b7c9d87f
george-593 <63100333+george-593@users.noreply.github.com>
2025-03-18 21:43:27 +0000

    Navbar improvements

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 
 import F1Logo from "../public/images/f1-logo.png";
 
+/* Component for the navbar at the top of all pages*/
 const Navbar = () => {
 	return (
-		<div className="bg-f1-black w-full py-4 flex justify-between items-center text-white">
+		// Flex and justify between to space items apart, item-center to keep them in the middle
+		<div className="bg-f1-black w-full py-4 px-8 flex justify-between items-center text-white">
 			{/*F1 Logo*/}
 			<Image
 				src={F1Logo}
 				width={250}
 				height={250}
 				alt="Formula One Logo"
-				className="px-4"
 			/>
 			{/*Placeholder until other pages are implemented*/}
-			<h1>F1 App</h1>
+			<h1 className="text-2xl font-bold"> F1 Data Dashboard</h1>
 		</div>
 	);
 };
