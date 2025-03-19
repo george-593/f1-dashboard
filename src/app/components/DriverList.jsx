58ec6f45aab6c4926bd31574cc76f9dc990ea8f6
george-593 <63100333+george-593@users.noreply.github.com>
2025-03-19 01:08:16 +0000

    Fetch drivers from the api, display them + fetch flags from another API

--- src/app/components/DriverList.jsx
+++ src/app/components/DriverList.jsx
@@ -3,12 +3,12 @@ import { useQuery } from "@tanstack/react-query";
 
 import DriverCard from "./ui/DriverCard";
 
-async function fetchAPI({ queryKey }) {
+const fetchAPI = async ({ queryKey }) => {
 	const [endpoint, params] = queryKey;
 	const res = await fetch(`https://api.openf1.org/v1/${endpoint}?${params}`);
 	if (!res.ok) throw new Error("Failed to fetch");
 	return res.json();
-}
+};
 
 const DriverList = () => {
 	const {
@@ -18,19 +18,17 @@ const DriverList = () => {
 	} = useQuery({
 		queryKey: ["drivers", "session_key=latest"],
 		queryFn: fetchAPI,
-		gcTime: 0,
 	});
 
 	if (isLoading) return <p>Loading Drivers...</p>;
 	if (error) return <p>Error fetching data for drives</p>;
-	console.table(driverData);
+	//console.table(driverData);
 
 	return (
-		<div>
-			{driverData.map((driver) => {
-				<DriverCard driver={driver} />;
-			})}
-			hi
+		<div className="">
+			{driverData.map((driver) => (
+				<DriverCard key={driver.driver_number} driver={driver} />
+			))}
 		</div>
 	);
 };
--- src/app/components/ui/DriverCard.jsx
+++ src/app/components/ui/DriverCard.jsx
@@ -1,6 +1,58 @@
-const DriverCard = ({ driver }) => {
+"use client";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchFlag = async ({ queryKey }) => {
+	const [countryCode] = queryKey;
+	const res = await fetch(
+		`https://restcountries.com/v3.1/alpha/${countryCode}?fields=flags`
+	);
+	if (!res.ok) throw new Error("Failed to fetch");
+	return res.json();
+};
+
+const DriverCard = ({ number, driver }) => {
+	const {
+		data: flagData,
+		isLoading,
+		error,
+	} = useQuery({
+		queryKey: [driver.country_code],
+		queryFn: fetchFlag,
+		enabled: !!driver.country_code,
+		gcTime: 0,
+	});
+
 	return (
-		<div className={`bg-[#${driver.team_colour}]`}>{driver.full_name}</div>
+		<div
+			style={{ backgroundColor: `#${driver.team_colour}` }}
+			className="py-2 px-4 rounded-lg my-2 flex"
+		>
+			<div className="relative text-center">
+				<img
+					src={driver.headshot_url}
+					alt={`${driver.full_name} headshot`}
+					className="w-32"
+				/>
+				<div className="w-full h-full absolute top-0 left-0 text-center text-white font-black text-9xl bg-black opacity-20 rounded-lg flex items-center justify-center">
+					{driver.driver_number}
+				</div>
+			</div>
+			<div className="ml-4 flex flex-col justify-between">
+				<div>
+					<h2 className="font-bold text-lg">
+						{driver.full_name.toUpperCase()}
+					</h2>
+					<h2 className="font-black text-lg">{driver.team_name}</h2>
+				</div>
+				{driver.country_code ? (
+					<img
+						src={flagData?.flags?.png}
+						alt={`${driver.country_code} flag`}
+						className="w-8 h-5"
+					/>
+				) : null}
+			</div>
+		</div>
 	);
 };
 
