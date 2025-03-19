c3b9a91f2d906024c460820e4179d2fa8d73ae6d
george-593 <63100333+george-593@users.noreply.github.com>
2025-03-19 12:15:31 +0000

    Do not make unnecesarry requests to the flags api

--- src/app/utils/fetchFlag.js
+++ src/app/utils/fetchFlag.js
@@ -1,6 +1,12 @@
 //Util function called by react-query to fetch flag data*/
 const fetchFlag = async ({ queryKey }) => {
 	const [countryCode] = queryKey;
+	{
+		/* Do not make a request to the API if the country code is null*/
+	}
+	if (!countryCode) {
+		throw new Error("Null country code");
+	}
 	const res = await fetch(
 		`https://restcountries.com/v3.1/alpha/${countryCode}?fields=flags`
 	);
